1b8e66311f29372acf9aa40b7c82f8c67626ee8e
Guilherme Guedes <167482095+gdGuedes@users.noreply.github.com>
2024-10-31 14:25:49 -0300

    Add files via upload

---
+++ reverterstring.cs
@@ -0,0 +1,18 @@
+﻿class InverterString
+{
+    static void Main()
+    {
+        Console.Write("Digite uma string para ser invertida: ");
+        string input = Console.ReadLine();
+
+        char[] caracteresInvertidos = new char[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            caracteresInvertidos[i] = input[input.Length - 1 - i];
+        }
+
+        string invertida = new string(caracteresInvertidos);
+        Console.WriteLine("String invertida: " + invertida);
+    }
+}
